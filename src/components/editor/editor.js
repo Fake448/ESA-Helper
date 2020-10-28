04324a7e2996bd31e83b21895942c07bcb99ea73
Kunze <kunzechristoph@esa-grimma.local>
2020-10-28 19:20:05 +0100

    first commit

---
+++ src/components/editor/editor.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Data from "../../data/data.json";
+
+function Editor() {
+  console.log(Data);
+
+  return (
+    <div>
+      Merkmale
+      <br />
+      <select>Editor</select>
+    </div>
+  );
+}
+
+
+export default Editor;
